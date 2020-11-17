efcb7edd2b502e2be3efb17526ab3e28b3c85425
dohuucong <60845588+dohuucong@users.noreply.github.com>
2020-11-17 07:38:28 +0700

    first commit

---
+++ Lap01-MVC/WingtipToys(1)/Models/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WingtipToys_1_.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() :base("WingtipToys_1_")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCarrtItem { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
